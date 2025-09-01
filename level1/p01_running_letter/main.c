5aa43cd521e9b7a6892cd0ece587ab5a6ee462c5
ghostzero-ai <261908127@qq.com>
2025-09-01 20:03:00 +0800

    Update main.c

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,39 @@
-#include <printf.h>
+#include <iostream>
+using namespace std;
 
 int main() {
-    printf("hello world!\n");
+	int a=30;
+    while(1){
+    	for(int i=0;i<=a;i++){
+    		for(int j=0;j<=i;j++){
+    			cout<<" ";
+			}
+			cout<<"a";
+			for(int j=i;j<=a;j++){
+    			cout<<" ";
+			}
+			system("cls");
+		}
+		
+		for(int i=0;i<=a;i++){
+    		for(int j=i;j<=a;j++){
+    			cout<<" ";
+			}
+			cout<<"a";
+			for(int j=0;j<=i;j++){
+    			cout<<" ";
+			}
+			system("cls");
+		}
+		
+	}
+    
+    
+    
+    
+    
+    
+
     return 0;
+    
 }
