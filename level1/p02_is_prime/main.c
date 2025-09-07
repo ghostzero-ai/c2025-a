3802da31da3433bc2d26a1cf28ec948f7183f1b0
ghostzero-ai <261908127@qq.com>
2025-09-07 20:13:01 +0800

    Update main.c

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,25 @@
-#include <printf.h>
-
+#include<stdlib.h>
+#include <stdio.h>
 int main() {
-    printf("hello world!\n");
+    long long su;
+    int flag=0;
+    scanf("%lld",&su);
+
+
+    for (int i=2;i*i<=su;i++) {
+        if (su%i==0) {
+
+            flag=1;
+            break;
+        }
+    }
+    if (flag==0) {
+        printf("Yes");
+    }else {
+        printf("No");
+    }
+
+
+
     return 0;
 }
